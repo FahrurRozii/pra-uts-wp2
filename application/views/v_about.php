66578834cfa2664075079c40c21392065174973a
adenovianto <adenovianto98@gmail.com>
2021-10-28 20:35:57 -0700

    Pra uts wp2

---
+++ application/views/v_about.php
@@ -0,0 +1,9 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <a>
+        NIM : 12201336 <br>
+        Nama : Fahrur Rozi <br>
+        Kelas : 12.3D.04 <br>
+        Asal Kampus : UBSI Bekasi (Cut Mutia)
+    </a>
+</section>
